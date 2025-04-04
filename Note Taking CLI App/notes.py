70b59c7054249a12eee66dde6f85251510d4e5bf
angelrxn00 <alpajares075@gmail.com>
2025-04-05 00:58:46 +0200

    v0.2 - Funcionalidad read_notes() añadida

--- Note Taking CLI App/notes.py
+++ Note Taking CLI App/notes.py
@@ -1,6 +1,11 @@
 # File -> notes.py
 # Here goes the core logic of the note app
 
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 def take_notes():
     """Prompt the user for a note and save it to a file"""
     with open("notes.txt", 'a') as notes:
@@ -9,9 +14,19 @@ def take_notes():
         notes.write(body + "\n")
 
 
+
+def read_notes():
+    """Reads the content of the current note"""
+    with open("notes.txt", 'r') as notes:
+        print(notes.readline())
+
 def main():
     take_notes()
-
-
+    logging.basicConfig(filename="notes.log", level=logging.INFO)
+    logger.info("Reading the notes")
+    read_notes()
+    logger.info("Notes read!")
+    
 if __name__ == '__main__':
     main()
+
